78578b08517d961ccbc22efa2702818e717e15cc
minori takanami <minoritakanami@minorinoMacBook-Air.local>
2025-08-28 13:17:58 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+body {
+  font-family: sans-serif;
+  background: #f4f4f4;
+  text-align: center;
+  padding: 30px;
+}
+
+.todo-container {
+  background: white;
+  padding: 20px;
+  border-radius: 10px;
+  display: inline-block;
+}
+
+li {
+  text-align: left;
+  margin: 5px 0;
+}
+
+button {
+  margin-left: 5px;
+}
